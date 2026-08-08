ab4430227bdb1fa31fdc45388f2a3cc7b0b57bb0
Henry Viera <henryviera2003@gmail.com>
2026-08-08 10:46:44 -0600

    Deploy: tabla de cuotas en escritorio Web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "54915821" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1810714977" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
