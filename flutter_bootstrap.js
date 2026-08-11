593194a3871f634630e6f071bdb9a32969c240b1
Henry Viera <henryviera2003@gmail.com>
2026-08-10 21:30:15 -0600

    Deploy: corrige clientes/prestamos invisibles por limit de 100

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "580182174" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3880813272" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
